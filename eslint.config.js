7245e147c45a2111f76ef253003cdec16c311f96
전민정 <minj_1203@naver.com>
2025-11-15 21:42:10 +0900

    chore: set eslint, prettier

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+    {
+        ignores: ['dist'],
+    },
+    {
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs.flat.recommended,
+            reactRefresh.configs.vite,
+            'prettier',
+        ],
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: {
+                ...globals.browser,
+                WorkerGlobalScope: 'readonly',
+            },
+        },
+        rules: {
+            'import/order': [
+                'error',
+                {
+                    groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+                    'newlines-between': 'always',
+                },
+            ],
+        },
+    },
+]);
