16b90554ca8cee17eca60d3ffe923bfabd4d40d7
전민정 <minj_1203@naver.com>
2025-11-18 17:10:08 +0900

    feat: test debounce performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Calculator from '@components/Calculator';
 import BaselinePanel from '@components/BaselinePanel';
+import DebouncePanel from '@components/DebouncePanel';
 
 function App() {
   return (
     <div className="flex items-center justify-center flex-col py-8 m-10">
       <Calculator />
+      <DebouncePanel />
       <BaselinePanel />
     </div>
   );
